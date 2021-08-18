dafc9b4d7a8491c7d7e89d6866a3f9af801559e5
fernando <warfernand@gmail.com>
2021-08-18 01:04:49 -0300

    Read News

---
+++ src/controllers/News/ReadNewsController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { ReadNewsService } from "../../services/News/ReadNewsService";
+
+class ReadNewsController {
+    async handle(request: Request, response: Response) {
+        const readNewsService = new ReadNewsService();
+
+        const news = await readNewsService.execute();
+
+        return response.json(news);
+    }
+}
+
+export { ReadNewsController };
--- src/routes/RoutesNews.ts
+++ src/routes/RoutesNews.ts
@@ -6,17 +6,20 @@ import { UpdateNewsController } from "../controllers/News/UpdateNewsController";
 import { CreateNewsController } from "../controllers/News/CreateNewsController";
 import { verifyTokenAuthentication } from "../middleware/CheckToken";
 import { DeleteNewsController } from "../controllers/News/DeleteNewsController";
+import { ReadNewsController } from "../controllers/News/ReadNewsController";
 
 const routerNews = Router();
 
 const createNewsController = new CreateNewsController();
 const updateNewsController = new UpdateNewsController();
+const readNewsController = new ReadNewsController();
 const deleteNewsController = new DeleteNewsController();
 
 const upload = multer(uploadConfig);
 
 routerNews.post('/createnews', upload.array('images'), verifyTokenAuthentication, createNewsController.handle);
 routerNews.put('/updatenews', upload.array('images'), verifyTokenAuthentication, updateNewsController.handle);
+routerNews.get('/readnews', upload.array('images'), verifyTokenAuthentication, readNewsController.handle);
 routerNews.delete('/deletenews', upload.array('images'), verifyTokenAuthentication, deleteNewsController.handle);
 
 export { routerNews }
